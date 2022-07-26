85bcb9c3a5a1fa61d51f09eb27dbb045c740109b
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-26 14:50:42 -0600

    Se agregó una función que calcula el dinero total ingresado por el usuario, se agregaron unit test

--- maquina-expendedora/src/Test/CalculateMoney.test.js
+++ maquina-expendedora/src/Test/CalculateMoney.test.js
@@ -1,4 +1,4 @@
-import { calculateTotalOrderCost } from "../Utils/calculateTotalMoney";
+import { calculateTotalOrderCost, calculateTotalMoneyForPay } from "../Utils/calculateTotalMoney";
 
 test('Calcula el total de la orden, con un solo tipo de refresco', () => {
   let order = [{
@@ -34,4 +34,74 @@ test('Calcula el total de la orden, si la orden es undefined', () => {
 test('Calcula el total de la orden, si la orden existe pero está vacía', () => {
   let order = [];
   expect(calculateTotalOrderCost(order)).toBe(0);
+});
+
+test('Calcula el total de dinero ingresado por el usuario', () => {
+  let moneyforPay = [
+    {
+      id: 0,
+      type: 1000,
+      quantity: 3
+    },
+    {
+      id: 1,
+      type: 500,
+      quantity: 1
+    },
+    {
+      id: 2,
+      type: 100,
+      quantity: 0
+    },
+    {
+      id: 3,
+      type: 50,
+      quantity: 1
+    },
+    {
+      id: 4,
+      type: 25,
+      quantity: 1
+    }];
+  expect(calculateTotalMoneyForPay(moneyforPay)).toBe(3575);
+});
+
+test('Calcula el total de dinero ingresado por el usuario, si el usuario no agregó ninguna moneda', () => {
+  let moneyforPay = [
+    {
+      id: 0,
+      type: 1000,
+      quantity: 0
+    },
+    {
+      id: 1,
+      type: 500,
+      quantity: 0
+    },
+    {
+      id: 2,
+      type: 100,
+      quantity: 0
+    },
+    {
+      id: 3,
+      type: 50,
+      quantity: 0
+    },
+    {
+      id: 4,
+      type: 25,
+      quantity: 0
+    }];
+  expect(calculateTotalMoneyForPay(moneyforPay)).toBe(0);
+});
+
+test('Calcula el total de dinero ingresado por el usuario, si el arreglo de monedas es undefined', () => {
+  let moneyforPay;
+  expect(calculateTotalMoneyForPay(moneyforPay)).toBe(0);
+});
+
+test('Calcula el total de dinero ingresado por el usuario, si el arreglo de monedas está vacío', () => {
+  let moneyforPay = [];
+  expect(calculateTotalMoneyForPay(moneyforPay)).toBe(0);
 });
