a9866c36884886d9c956e9c1535b148fb219b935
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-26 12:29:13 -0600

    Agregado el componente vending machine donde se llamaran los demás componentes

---
+++ maquina-expendedora/src/Components/VendingMachine/VendingMachine.js
@@ -0,0 +1,18 @@
+import './VendingMachine.css'
+
+
+export const VendingMachine = () => {
+  return (
+    <>
+      <div className='header'>
+        <h2> Bebidas </h2>
+      </div>
+      <div className='main-container'>
+        <div className='principal-menu'>
+          <h2 className='title'> Productos disponibles </h2>
+
+        </div>
+      </div>
+    </>
+  );
+};
