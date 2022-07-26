53e455dc4942f28219916d0d1334098a27247f27
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-26 12:15:09 -0600

    Agregados los datos iniciales que la aplicación usará para cargar información

---
+++ maquina-expendedora/src/data.js
@@ -0,0 +1,80 @@
+export const machineStock = [
+  {
+    id: 0,
+    type: 'Coca Cola',
+    price: 500,
+    quantity: 10,
+    image: 'CocaCola.jpg'
+  },
+  {
+    id: 1,
+    type: 'Pepsi',
+    price: 600,
+    quantity: 8,
+    image: 'Pepsi.jpg'
+  },
+  {
+    id: 2,
+    type: 'Fanta',
+    price: 550,
+    quantity: 10,
+    image: 'Fanta.jpg'
+  },
+  {
+    id: 3,
+    type: 'Sprite',
+    price: 725,
+    quantity: 15,
+    image: 'Sprite.jpg'
+  }
+];
+
+export const machineChange = [
+  {
+    id: 1,
+    type: '500',
+    quantity: 20
+  },
+  {
+    id: 2,
+    type: '100',
+    quantity: 30
+  },
+  {
+    id: 3,
+    type: '50',
+    quantity: 50
+  },
+  {
+    id: 4,
+    type: '25',
+    quantity: 25
+  },
+];
+
+export const initialClientMoney = [
+  {
+    id: 0,
+    type: 1000,
+    quantity: 0
+  },
+  {
+    id: 1,
+    type: 500,
+    quantity: 0
+  },
+  {
+    id: 2,
+    type: 100,
+    quantity: 0
+  },
+  {
+    id: 3,
+    type: 50,
+    quantity: 0
+  },
+  {
+    id: 4,
+    type: 25,
+    quantity: 0
+  }]
