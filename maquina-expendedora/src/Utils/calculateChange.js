0772736ac4c58cea47a008b4de640ddb382101d8
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-26 16:02:36 -0600

    Agregada una función que calcula el desglose del vuelto y otra que pasa ese desglose a string, a ambas se le agregaron unit testing

--- maquina-expendedora/src/Utils/calculateChange.js
+++ maquina-expendedora/src/Utils/calculateChange.js
@@ -7,3 +7,27 @@ export const calculateTotalChange = (totalOrderCost, totalMoneyForPay) => {
   return totalChange;
 };
 
+export const calculateItemizedChange = (totalChange, coinsStock) => {
+  let itemizedChange = [];
+  let change = totalChange;
+  if (totalChange !== undefined && coinsStock !== undefined) {
+    for (let i = 0; change > 0 && i < coinsStock.length; i++) {
+      let coin = coinsStock[i];
+
+      if (coin.type <= change) {
+        const coinsNeeded = Math.floor(change / coin.type);
+        if (coin.quantity >= coinsNeeded) {
+          const coinType = coin.type;
+          itemizedChange.push(
+            {
+              type: coinType,
+              quantity: coinsNeeded
+            }
+          )
+          change -= coinType * coinsNeeded;
+        }
+      }
+    }
+  }
+  return itemizedChange;
+};
